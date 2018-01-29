ff4de092424299f839ddbc6093daf33c8d982b8a
Francisco Paisana <paisanaf@tcd.ie>
2018-01-29 12:29:24 +0000

    corrected some minor bugs. LTE seems ready to fly

--- python/labeling_framework/waveform_generators/lte_source.py
+++ python/labeling_framework/waveform_generators/lte_source.py
@@ -25,8 +25,8 @@ logger = logging_utils.DynamicLogger(__name__)
 
 prb_mapping = {6: 128, 15: 256, 25: 384, 50: 768, 75: 1024, 100: 1536}
 fftsize_mapping = {128: 1.4e6, 256: 3e6, 384: 5.0e6, 768: 10.0e6, 1024: 15.0e6, 1536: 20.0e6}
-ZC_cached = {
-}
+ZC_cached = {}
+bw_cached = {}
 
 def compute_LTE_ZC(fft_size):
     if fft_size in ZC_cached:
@@ -57,7 +57,7 @@ def lte_frame_window(peak,samp_rate):
     pss_offset = int(np.round(406.9e-6*samp_rate))
     return (peak-pss_offset,peak+frame_dur-pss_offset)
 
-def find_lte_frame_windows(x,fft_size,samp_rate):
+def find_lte_frame_windows(x,fft_size):
     l = []
     half_frame_dur=5.0e-3*20.0e6+1
     peaks = find_zc_peaks(x,fft_size)
@@ -79,6 +79,44 @@ def find_lte_frame_windows(x,fft_size,samp_rate):
             else:
                 raise RuntimeError('Couldnt find the second PSSS peak of the LTE frame')
     return l
+
+def merge_boxes_within_same_lte_frame(x,tfreq_boxes_x,fft_size):
+    tfreq_boxes = copy.deepcopy(tfreq_boxes_x)
+    frame_win_list = find_lte_frame_windows(x,fft_size)
+    if len(frame_win_list)==0:
+        raise RuntimeError('Couldnt find any ZC peak')
+    new_tfreq_boxes = []
+    # NOTE: This avoids eliminating boxes for which frames were not found.
+    # They are considered last, so there are no common boxes between them and other frames
+    begin_truncate = (0,frame_win_list[0][0])
+    end_truncate = (frame_win_list[-1][1]+1,len(x))
+    frame_win_list.append(begin_truncate)
+    frame_win_list.append(end_truncate) # this includes boxes for which frame was not found
+    for tframe in frame_win_list:
+        frame_boxes = list(tfbox.select_boxes_that_intersect_section(tfreq_boxes,tframe))
+        if len(frame_boxes)==0:
+            continue
+        maxpwr = np.max([b.params['power'] for b in frame_boxes])
+        mintstamp = np.min([b.time_bounds[0] for b in frame_boxes])
+        maxtstamp = np.max([b.time_bounds[1] for b in frame_boxes])
+        minfreq = np.min([b.freq_bounds[0] for b in frame_boxes])
+        maxfreq = np.max([b.freq_bounds[1] for b in frame_boxes])
+        # caches the measured BW. If we capture a box that does not fit this BW
+        # (CC may have smaller BW), we correct it
+        if np.abs(maxfreq+minfreq)>1e-5: # I do not expect CFO here
+            raise AssertionError('I do not expect CFO here. However, I got ({},{})'.format(minfreq,maxfreq))
+        if fft_size not in bw_cached or (maxfreq-minfreq)>(bw_cached[fft_size][1]-bw_cached[fft_size][0]):
+            bw_cached[fft_size] = (minfreq,maxfreq)
+        minfreq,maxfreq = bw_cached[fft_size]
+        if tframe!=begin_truncate:
+            maxtstamp = max(maxtstamp,tframe[1])
+        if tframe!=end_truncate:
+            mintstamp = min(mintstamp,tframe[0])
+        new_box = tfbox.TimeFreqBox((mintstamp,maxtstamp),(minfreq,maxfreq),'lte')
+        new_box.params['power'] = maxpwr
+        new_tfreq_boxes.append(new_box)
+        tfreq_boxes = [b for b in tfreq_boxes if b.time_intersection(tframe)==None]
+    return new_tfreq_boxes
 
 class GrLTETracesFlowgraph(gr.top_block):
     # NOTE: srsLTE does not use the standard mapping
@@ -196,26 +234,29 @@ def run(args):
             # merge boxes if broadcast channel is empty
             metadata = sda.get_stage_derived_parameter(v, 'spectrogram_img_metadata', args['stage_name'])
             tfreq_boxes = copy.deepcopy(metadata.tfreq_boxes)
-            frame_win_list = find_lte_frame_windows(gen_data,tb.fft_size,tb.samp_rate)
-            if len(frame_win_list)==0:
-                raise RuntimeError('Couldnt find any ZC peak')
-            new_tfreq_boxes = []
-            # NOTE: This avoids eliminating boxes for which frames were not found. They are considered last, so there are no common boxes between frames
-            frame_win_list.append((0,frame_win_list[0][0]))
-            frame_win_list.append((frame_win_list[-1][1]+1,len(gen_data))) # this includes boxes for which frame was not found
-            for tframe in frame_win_list:
-                frame_boxes = list(tfbox.select_boxes_that_intersect_section(tfreq_boxes,tframe))
-                if len(frame_boxes)==0:
-                    continue
-                maxpwr = np.max([b.params['power'] for b in frame_boxes])
-                mintstamp = np.min([b.time_bounds[0] for b in frame_boxes])
-                maxtstamp = np.max([b.time_bounds[1] for b in frame_boxes])
-                minfreq = np.min([b.freq_bounds[0] for b in frame_boxes])
-                maxfreq = np.max([b.freq_bounds[1] for b in frame_boxes])
-                new_box = tfbox.TimeFreqBox((mintstamp,maxtstamp),(minfreq,maxfreq),'lte')
-                new_box.params['power'] = maxpwr
-                new_tfreq_boxes.append(new_box)
-                tfreq_boxes = [b for b in tfreq_boxes if b.time_intersection(tframe)==None]
+            new_tfreq_boxes = merge_boxes_within_same_lte_frame(gen_data,
+                tfreq_boxes,tb.fft_size)
+            # frame_win_list = find_lte_frame_windows(gen_data,tb.fft_size,tb.samp_rate)
+            # if len(frame_win_list)==0:
+            #     raise RuntimeError('Couldnt find any ZC peak')
+            # new_tfreq_boxes = []
+            # # NOTE: This avoids eliminating boxes for which frames were not found.
+            # # They are considered last, so there are no common boxes between them and other frames
+            # frame_win_list.append((0,frame_win_list[0][0]))
+            # frame_win_list.append((frame_win_list[-1][1]+1,len(gen_data))) # this includes boxes for which frame was not found
+            # for tframe in frame_win_list:
+            #     frame_boxes = list(tfbox.select_boxes_that_intersect_section(tfreq_boxes,tframe))
+            #     if len(frame_boxes)==0:
+            #         continue
+            #     maxpwr = np.max([b.params['power'] for b in frame_boxes])
+            #     mintstamp = np.min([b.time_bounds[0] for b in frame_boxes])
+            #     maxtstamp = np.max([b.time_bounds[1] for b in frame_boxes])
+            #     minfreq = np.min([b.freq_bounds[0] for b in frame_boxes])
+            #     maxfreq = np.max([b.freq_bounds[1] for b in frame_boxes])
+            #     new_box = tfbox.TimeFreqBox((mintstamp,maxtstamp),(minfreq,maxfreq),'lte')
+            #     new_box.params['power'] = maxpwr
+            #     new_tfreq_boxes.append(new_box)
+            #     tfreq_boxes = [b for b in tfreq_boxes if b.time_intersection(tframe)==None]
             metadata.tfreq_boxes = new_tfreq_boxes
             sda.set_stage_derived_parameter(v, args['stage_name'], 'spectrogram_img_metadata', metadata)
         except RuntimeError, e:
