06fc054b8377e2a6ffcb78e2b1b3b4c9bb8bbc0a
connect <paisanaf@tcd.ie>
2018-02-15 17:48:03 +0000

    Now I can save/load the best configuration so far. I am also altering the format of the config files

--- python/darkflow_tools/parse_yolo_cfg.py
+++ python/darkflow_tools/parse_yolo_cfg.py
@@ -6,16 +6,180 @@ import os
 import configparser
 import shutil
 
-# from labeling_framework.utils.filesystem_utils import *
+# helper
+def try_mkdir(path):
+    if not os.path.exists(path):
+        if os.path.isfile(path):
+            raise RuntimeError('The path {} is an existing file'.format(path))
+        os.mkdir(path)
+
+class DarkflowArgs(object): # to replace YOLOCfgPaths in the future
+    option_types = {'batch':int,
+                    'gpu':float,
+                    'lr':float,
+                    'trainer':str,
+                    'threshold':float,
+                    'momentum':float,
+                    'dataset':str}
+    img_foldername = 'Images'#'JPEGImages'
+    annotations_foldername = 'Annotations'
+    def __init__(self):
+        # train/predict params (setting defaults here)
+        # NOTE: None means darkflow default
+        self.batch = 16
+        self.gpu = 0.0
+        self.lr = 1.0e-5
+        self.trainer = None
+        self.threshold = None
+        self.momentum = 0.9
+        self.train = False
+
+    def try_setattr(self,name,value,type_cast=None):
+        if value is not None: # overwrites if specified.
+            if type_cast is None:
+                if name in DarkflowArgs.option_types:
+                    setattr(self, name, DarkflowArgs.option_types[name](value)) # cast
+                else:
+                    setattr(self, name, value) # no cast
+            else:
+                setattr(self, name, type_cast(value)) # specified cast
+
+    def read_yaml(self,yaml_file):
+        # real file
+        with open(yaml_file,'r') as f:
+            yaml_dict = yaml.load(f)
+        yaml_path = os.path.basename(yaml_file)
+
+        def abspath(relative_path):
+            return os.path.normpath(os.path.join(yaml_path,relative_path))
+
+        # set paths
+        dataset_path = abspath(yaml_dict['dataset']['dataset'])
+        tmp_path = abspath(yaml_dict['dataset']['tmp_folder'])
+        model_path = abspath(yaml_dict['model']['model_path'])
+        self.set_paths(model_path,dataset_path,tmp_path)
+
+        # set darkflow args
+        darkflow_args = yaml_dict['darkflow']
+        for k,v in darkflow_args.items():
+            self.try_setattr(k,v,DarkflowArgs.option_types[k])
+        
+    def set_paths(self,model_path,dataset_path,tmp_path):
+        model_path = os.path.abspath(os.path.expanduser(model_path))
+        self.try_setattr('model',model_path)
+        # dataset folder
+        dataset_path = os.path.abspath(os.path.expanduser(dataset_dir))
+        self.try_setattr('dataset',dataset_path)
+        annotation_path = os.path.join(dataset_path,DarkflowArgs.annotations_foldername)
+        self.try_setattr('annotation',annotation_path)
+        imgdir_path = os.path.join(dataset_path,DarkflowArgs.img_foldername)
+        self.try_setattr('imgdir',imgdir_path)
+
+        # tmp folder
+        tmp_path = os.path.abspath(os.path.expanduser(tmp_dir))
+        labels_path = os.path.join(tmp_path,'labels.txt')
+        self.try_setattr('labels',labels_path)
+        backup_path = os.path.join(tmp_path,'ckpt')
+        self.try_setattr('backup',backup_path)
+        bin_path = os.path.join(tmp_path,'bin')
+        self.try_setattr('bin',bin_path)
+        summary_path = os.path.join(tmp_path,'summary')
+        self.try_setattr('summary',summary_path)
+        
+    def parse_cmdline(self,cmd_args,type_cast):
+        options = vars(cmd_args)
+        for name,value in options.items():
+            if name.startswith('__') or name=='mode': # it is private #FIXME
+                continue
+            self.try_setattr(name,value,DarkflowArgs.option_types[name])
+        
+    def generate_args(self):
+        assert_paths_exist(self)
+        options = vars(self)
+        for k,v in options.items():
+            if v is None: # Remove any None
+                del options[k]
+        return options
+
+def assert_paths_exist(cfg_obj):
+    def assert_path_exists(path,check_file=False):
+        test = not os.path.exists(path)
+        test |= check_file and not os.path.isfile(path)
+        if test:
+            raise AssertionError('Path {} does not exist.'.format(path))
+    # assert all paths exist
+    assert_path_exists(cfg_obj.model)
+    assert_path_exists(cfg_obj.dataset)
+    assert_path_exists(cfg_obj.annotation)
+    assert_path_exists(cfg_obj.imgdir)
+    assert_path_exists(cfg_obj.labels)
+    assert_path_exists(cfg_obj.backup)
+    assert_path_exists(cfg_obj.bin)
+    assert_path_exists(cfg_obj.summary)
+
+class DarkflowConfig(object):
+    darknet_annotations_foldername = 'darknet_annotations'
+    def __init__(self):
+        self.args = None
+        
+    def tmp_path(self):
+        return os.path.basename(self.args.backup)
+
+    def darknet_annotations_path(self):
+        return os.path.join(self.tmp_path(),DarkflowConfig.darknet_annotations_foldername)
+
+    def setup_paths(self):
+        # setup tmp folder which is going to be used as output
+        try_mkdir(self.tmp_path())
+        # setup ckpt folder
+        try_mkdir(self.args.backup)
+        # make out dir inside images
+        try_mkdir(os.path.join(self.args.imgdir,'out'))
+
+def assert_config_correctness():
+    # assert param correctness
+    cfg_params = yolo_cfg.cfg_params
+    model_params = cfg_params['model']
+
+    # read cfg file
+    cfgparser = configparser.ConfigParser(strict=False)
+    cfgparser.read(yolo_cfg.model_path())
+
+    # assert height/width correctness
+    w = cfgparser.getint('net','width')
+    h = cfgparser.getint('net','height')
+    assert w==model_params['width'] and h==model_params['height']
+
+    # assert num of classes is consistent
+    num_classes = cfgparser.getint('region','classes')
+    if num_classes != len(model_params['classes']):
+        raise AssertionError('ERROR: Make sure the num of classes in the model cfg file is the same as in the yaml file')
+
+    # assert the number of anchors is consistent
+    num_anchors = cfgparser.getint('region','num')
+    if num_anchors != model_params['num_anchors']:
+        raise AssertionError('ERROR: Make sure the num of anchors in the model cfg file is the same as in the yaml file')
+
+    # assert that the number of filters is correct
+    # NOTE: last convolutional layer wins in the parsing
+    num_filters = cfgparser.getint('convolutional','filters')
+    expected_num_filters = num_anchors*(num_classes+5)
+    if num_filters!=expected_num_filters:
+        raise AssertionError('ERROR: Set the number of filters in the last conv layer to {}'.format(expected_num_filters))
+
+    # TODO: check if the number of anchors is equal to num
 
 class YOLOCfgPaths(object):
     img_foldername = 'Images'#'JPEGImages'
     annotations_foldername = 'Annotations'
     darknet_annotations_foldername = 'darknet_annotations'
 
-    def __init__(self,cfg_params, yml_file):
+    def __init__(self,yml_cfg_args, yml_file):
+        """
+        Receives the parsed YAML parameters, and the name of the yaml file
+        """
         self.yml_filepath = os.path.abspath(yml_file)
-        self.cfg_params = dict(cfg_params)
+        self.cfg_params = dict(yml_cfg_args)
         self.yml_path = os.path.dirname(self.yml_filepath)
         self.setup_paths()
         self.assert_validity()
@@ -79,9 +243,12 @@ class YOLOCfgPaths(object):
         return '{}/{}'.format(self.tmp_path(),'ckpt')
 
 def read_yaml_main_config(filename):
+    """
+    Reads a YAML file with the configuration of the YOLO network
+    """
     with open(filename,'r') as f:
-        cfg_params = yaml.load(f)
-    return YOLOCfgPaths(cfg_params,filename)
+        cfg_args = yaml.load(f)
+    return YOLOCfgPaths(cfg_args, filename)
 
 def assert_yaml_cfg_correctness(yolo_cfg):
     cfg_params = yolo_cfg.cfg_params
